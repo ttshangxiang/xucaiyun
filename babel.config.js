c423123a102144540f8c7856dcf0f60a8f0f5010
jianghuaqing <jianghuaqing@haizhi.com>
2019-01-17 08:57:35 +0800

    babel-classes

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
     // '@babel/plugin-syntax-typescript',
     // ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     // ['@babel/plugin-proposal-class-properties', { 'loose' : true }],
+    '@babel/plugin-transform-classes',
     ['@babel/plugin-transform-runtime', {
       corejs: 2
     }]
